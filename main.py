af4116b61146c2c33270259c407550e4499328c1
root <root@srv890502.hstgr.cloud>
2025-07-02 18:15:35 +0000

    Auto sync from /signup

--- main.py
+++ main.py
@@ -542,4 +542,4 @@ async def bulk_upload_quiz(data: BulkUploadRequest):
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, from FastAPI!"}
+    return {"message": "Hell from FastAPI!"}
